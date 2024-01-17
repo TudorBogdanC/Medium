2c479b432a32654d35c3093119eff8b48dd7c2ae
“TudorBogdanC” <toriatteteo@gmail.com>
2024-01-17 21:03:26 +0100

    update

--- Medium/src/App.jsx
+++ Medium/src/App.jsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import CustomHeader from './components/organisms/CustomHeader'
 import CustomMain from './components/organisms/CustomMain';
-import CustomFooter from './components/organisms/CustomFooter';
+
 
 function App() {
   
